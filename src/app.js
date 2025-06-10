fec7cd28ed5e2a16d22645fa5beced3830990669
S0MBRA99 <167691743+S0MBRA99@users.noreply.github.com>
2025-06-10 08:13:14 +0000

    first-commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const excuses = {
+    who : ['The dog', 'My grandma', 'The mailman', 'My bird'],
+    action : ['ate', 'peed', 'crushed', 'broke'],
+    what : ['my homework', 'my phone', 'the car'],
+    when : ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying']
 };
+
+function generarExcusa(){
+    let completeSentence = ""
+
+    for (let categoria in excuses) {
+    
+        let randomNumber = Math.floor(Math.random()* excuses[categoria].length) 
+        let randomString = excuses[categoria][randomNumber]
+        completeSentence += (completeSentence ? " ":"") + randomString
+    }
+    return completeSentence
+}
+window.onload = function(){
+  document.querySelector(".excuse").innerHTML = `<p>${generarExcusa()}</p>`;
+}
